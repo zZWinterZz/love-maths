16f0bd0ab8094777adcf823ea407c31e1f46dcb6
zZWinterZz <ryanwinter1989@gmail.com>
2025-08-12 12:35:53 +0100

    add divide gametype add data types to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,8 @@ function runGame(gameType) {
     displayMultiplyQuestion(num1, num2);
   } else if (gameType === "subtract") {
     displaySubtractQuestion(num1, num2);
+  } else if (gameType === "divide") {
+    displayDivideQuestion(num1, num2);
   } else {
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType}. Aborting!`;
@@ -87,6 +89,8 @@ function calculateCorrectAnswer() {
     return [operand1 * operand2, "multiply"];
   } else if (operator === "-") {
     return [operand1 - operand2, "subtract"];
+  } else if (operator === "/") {
+    return [operand1 / operand2, "divide"];
   } else {
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator ${operator}. Aborting!`;
@@ -127,4 +131,9 @@ function displayMultiplyQuestion(operand1, operand2) {
   document.getElementById("operand1").textContent = operand1;
   document.getElementById("operand2").textContent = operand2;
   document.getElementById("operator").textContent = "x";
+}
+function displayDivideQuestion(operand1, operand2) {
+  document.getElementById("operand1").textContent = operand1 * operand2;
+  document.getElementById("operand2").textContent = operand2;
+  document.getElementById("operator").textContent = "/";
 }
